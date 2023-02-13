8dc8746c9d30264b38b56792b5fad0d7dd6d0a4d
Bartłomiej Sereda <bartlomiej.sereda@microsoft.wsei.edu.pl>
2023-02-13 22:31:12 +0100

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [MaxLength(length:200)]
+        public string Description { get; set; }
+        public string Language { get; set; }
+        [Required, MaxLength(length:13)]
+        public string ISBN { get; set; }
+        [Required, DataType(DataType.Date),Display(Name = "Published Date")]
+        public DateTime PublishedDate { get; set; }
+        [Required, DataType(DataType.Currency)]
+        public int Price { get; set; }
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; }
+   
+
+    }
+}
