819bf3a46130b47b7a7a3564c7378beddb33a51e
Bartłomiej Sereda <bartlomiej.sereda@microsoft.wsei.edu.pl>
2023-02-17 02:42:24 +0100

    More books added to data seed.

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -41,13 +41,54 @@ namespace BookShop.Models
                    {
                        Title = "Harry Potter and the Philosopher's Stone",
                        Language = "English",
-                       ISBN = "9789129688313",
-                       PublishedDate = DateTime.Parse("2002-1-18"),
-                       Price = 56,
+                       ISBN = "9789129688311",
+                       PublishedDate = DateTime.Parse("1997-6-26"),
+                       Price = 30,
                        Author = "J.K. Rowling",
                        ImageUrl = "/images/harry1.jpg"
-                   }
+                   },
+                    new Book
+                    {
+                        Title = "Harry Potter And The Chamber Of Secrets",
+                        Language = "English",
+                        ISBN = "9780261102352",
+                        PublishedDate = DateTime.Parse("1998-7-2"),
+                        Price = 31,
+                        Author = "J.K. Rowling",
+                        ImageUrl = "/images/harry2.jpg"
+                    },
+                    new Book
+                    {
+                        Title = "The Lord Of The Rings",
+                        Language = "English",
+                        ISBN = "9780261102353",
+                        PublishedDate = DateTime.Parse("2008-3-4"),
+                        Price = 100,
+                        Author = "J. R. R. Tolkien",
+                        ImageUrl = "/images/lord.jpg"
+                    },
+                    new Book
+                    {
+                        Title = "The hobbit, or There and back again",
+                        Language = "English",
+                        ISBN = "9780261102354",
+                        PublishedDate = DateTime.Parse("2012-7-12"),
+                        Price = 120,
+                        Author = "J. R. R. Tolkien",
+                        ImageUrl = "/images/hobbit.jpg"
+                    },
+                    new Book
+                    {
+                        Title = "Fairy Tale",
+                        Language = "English",
+                        ISBN = "9780261102355",
+                        PublishedDate = DateTime.Parse("2012-12-5"),
+                        Price = 59,
+                        Author = "Stephen King",
+                        ImageUrl = "/images/fairytale.jpg"
+                    }
                 );
+
                 
                 context.SaveChanges();
             }
