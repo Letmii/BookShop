91d5217a0774739a759355d39638da1a0bdb9295
Bartłomiej Sereda <bartlomiej.sereda@microsoft.wsei.edu.pl>
2023-02-18 18:44:14 +0100

    Order and checkout

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,69 @@
+﻿using BookShop.Data;
+using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private readonly BookShopContext _context;
+        private readonly Cart _cart;
+
+        public OrderController(BookShopContext context, Cart cart)
+        {
+            _context = context;
+            _cart = cart;
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var cartItems = _cart.GetAllCartItems();
+            _cart.CartItems = cartItems;
+
+            if (_cart.CartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Cart is empty, please add a book first.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                CreateOrder(order);
+                _cart.ClearCart();
+                return View("CheckoutComplete", order);
+            }
+
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete(Order order)
+        {
+            return View(order);
+        }
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+
+            var cartItems = _cart.CartItems;
+
+            foreach (var item in cartItems)
+            {
+                var orderItem = new OrderItem()
+                {
+                    Quantity = item.Quantity,
+                    BookId = item.Book.Id,
+                    OrderId = order.Id,
+                    Price = item.Book.Price * item.Quantity
+                };
+                order.OrderItems.Add(orderItem);
+                order.OrderTotal += orderItem.Price;
+            }
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+        }
+    }
+}
